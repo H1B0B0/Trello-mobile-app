fc9472d1ce0d814e862a196d965a01310e35e798
etienne <etienne.mentrel@scafe.io>
2024-03-25 12:00:56 +0100

    Remove LogBox ignoreAllLogs call in App.test.js

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,11 +1,9 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import App from "../App";
-import { LogBox } from "react-native";
 
 describe("<App />", () => {
   it("renders correctly", () => {
-    jest.spyOn(LogBox, "ignoreAllLogs").mockImplementation(() => {});
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
